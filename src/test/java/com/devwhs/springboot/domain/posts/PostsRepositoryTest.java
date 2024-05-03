113fef78ba2eace9bd934015de6b76b923493a5b
Wonhee Son <dnjsgml1230@gmail.com>
2024-05-03 13:38:47 +0900

    test: 게시물 도메인

--- src/test/java/com/devwhs/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/devwhs/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,4 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
-class PostsRepositoryTest {
-  
+package com.devwhs.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시물_저장_및_불러오기() {
+        // given
+        String title = "테스트 게시물";
+        String content = "테스트 본문";
+
+        postsRepository.save(
+                Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("heextory@icloud.com")
+                        .build()
+        );
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
 }
